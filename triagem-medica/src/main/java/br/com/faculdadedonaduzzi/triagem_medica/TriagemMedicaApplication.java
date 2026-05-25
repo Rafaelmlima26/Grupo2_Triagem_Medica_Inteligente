91db1424e75de38b6bd61c3643e58847d520f2d4
Rafaelmlima201 <rafaelmlima201@gmail.com>
2026-05-25 20:26:55 -0300

    feat: setup inicial do projeto - Spring Boot + PostgreSQL

---
+++ triagem-medica/src/main/java/br/com/faculdadedonaduzzi/triagem_medica/TriagemMedicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.faculdadedonaduzzi.triagem_medica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriagemMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriagemMedicaApplication.class, args);
+	}
+
+}
---
+++ triagem-medica/src/test/java/br/com/faculdadedonaduzzi/triagem_medica/TriagemMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.faculdadedonaduzzi.triagem_medica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriagemMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
